4aa6e6d8260931ff40b4ffd1ed3a416d37e53508
Waleed Smadi <waleedsmadi95@gmail.com>
2025-04-28 12:05:49 +0300

    Add ContactForm component

---
+++ src/component/ContactForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+function ContactForm({ addContact }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !email) {
+      alert('Please fill out all fields');
+      return;
+    }
+    addContact({ name, email });
+    setName('');
+    setEmail('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button type="submit">Add Contact</button>
+    </form>
+  );
+}
+
+export default ContactForm;
